7acaf5d380e217a94e40e3ec6c5e056647176632
MineDragonCZ <MineDragonCZ@email.cz>
2025-09-25 10:34:55 +0200

    Fixed JEI issue with our GUI

--- src/main/java/dev/tauri/rsjukeboxes/screen/container/TieredJukeboxGui.java
+++ src/main/java/dev/tauri/rsjukeboxes/screen/container/TieredJukeboxGui.java
@@ -52,7 +52,7 @@ public class TieredJukeboxGui extends AbstractContainerScreen<TieredJukeboxConta
     public void render(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
         GuiHelper.graphics = graphics;
         RenderSystem.disableDepthTest();
-        renderTransparentBackground(graphics, this);
+        renderBackground(graphics);
 
         super.render(graphics, mouseX, mouseY, partialTicks);
         renderTooltip(graphics, mouseX, mouseY);
